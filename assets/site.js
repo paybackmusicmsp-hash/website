db4dd9ee2e110c6647b48d4f3cf13c1ca38148d2
existance-is-futile <seth4758@gmail.com>
2026-04-15 19:53:37 -0700

    Use MSP logo for shared brand mark

--- assets/site.js
+++ assets/site.js
@@ -29,7 +29,9 @@ window.MSPSite = (() => {
       header.innerHTML = `
         <div class="site-topbar">
           <a class="brand-lockup" href="index.html" aria-label="MSP 790 Payback Music home">
-            <span class="brand-mark">790</span>
+            <span class="brand-mark" aria-hidden="true">
+              <img src="assets/favicon.svg" alt="">
+            </span>
             <span class="brand-text">
               <strong>MSP 790</strong>
               <span>Payback Music</span>
